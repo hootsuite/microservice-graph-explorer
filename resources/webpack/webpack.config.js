0e8e5f103a63090bf9ebb5718f51fbd61e588e16
Adam Arsenault <adam.arsenault@hootsuite.com>
2019-07-03 16:01:27 -0700

    Update webpack and babel to get rid of security issues (#9)

--- resources/webpack/webpack.config.js
+++ resources/webpack/webpack.config.js
@@ -1,7 +1,6 @@
 var rootDir = require('path').resolve(__dirname, '../../');
 
 var jsDir = rootDir + '/src';
-var buildDir = rootDir + '/__build__';
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -12,20 +11,18 @@ module.exports = {
   },
 
   output: {
-    path: './public/javascripts/build',
+    path: rootDir + '/public/javascripts/build',
     filename: '[name].js',
     publicPath: './public'
   },
 
-  distDir: buildDir,
-
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx']
+    extensions: ['.webpack.js', '.web.js', '.js', '.jsx']
   },
 
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.(png|svg|woff|woff2|eot|ttf|otf)/, loader: 'url-loader?limit=100000' },
       { test: /\.less/, loader: 'style-loader!css-loader!less-loader' }
